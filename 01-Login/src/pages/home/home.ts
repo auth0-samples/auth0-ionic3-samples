9a05d54832de3fa3d4c6f255429a45299b0be611
Kim Maida <kim.maida@auth0.com>
2018-01-15 13:44:26 -0500

    Clean up unnecessary subscription.

--- 01-Login/src/pages/home/home.ts
+++ 01-Login/src/pages/home/home.ts
@@ -1,15 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { AuthService } from './../../services/auth.service';
-import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  loginSub: Subscription;
-  loggedIn: boolean;
 
   constructor(
     public navCtrl: NavController,
